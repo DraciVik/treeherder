7654f97459ad2dff2b0017d18e9f37061ead7ee8
DraciVik <viktor.stojanov90@gmail.com>
2019-10-29 18:50:17 +0100

    Fixed jsx-a11y rules

--- ui/push-health/ClassificationGroup.jsx
+++ ui/push-health/ClassificationGroup.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -76,16 +74,18 @@ class ClassificationGroup extends React.PureComponent {
 
     return (
       <Row className={`justify-content-between ${className}`}>
-        <h4 className="w-100" onClick={this.toggleDetails}>
-          <span className={`pointable badge badge-${headerColor} w-100`}>
-            {name} : {Object.keys(group).length}
-            <FontAwesomeIcon
-              icon={expandIcon}
-              className="ml-1"
-              title="expand"
-            />
-          </span>
-        </h4>
+        <Button className="w-100" onClick={this.toggleDetails}>
+          <h4 className="w-100">
+            <span className={`pointable badge badge-${headerColor} w-100`}>
+              {name} : {Object.keys(group).length}
+              <FontAwesomeIcon
+                icon={expandIcon}
+                className="ml-1"
+                title="expand"
+              />
+            </span>
+          </h4>
+        </Button>
         <Collapse isOpen={detailsShowing} className="w-100">
           {hasRetriggerAll && (
             <ButtonGroup>
